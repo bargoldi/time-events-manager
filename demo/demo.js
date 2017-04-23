4bc877f22fbb59e8a098a923ee530abe9f89f9ee
Ziv <polackziv@gmail.com>
2017-04-23 13:08:43 +0300

    Added a few demo cases

--- demo/demo.js
+++ demo/demo.js
@@ -2,8 +2,17 @@ var firstTimeoutID = setTimeout(function () {
     console.log('First timeout, after 5 seconds');
 }, 5000);
 
+var secondTimeoutID = setTimeout(function () {
+    console.log('second timeout, after 8 seconds');
+}, 8000);
+
 // Asserting validation of comparison between ID getter and index getter
 console.assert(timeoutCollection.get(0) === timeoutCollection.getById(firstTimeoutID), 'TimeoutCollection object - index against id');
 
+console.log(timeoutCollection.get(0)); //timeout object
+
 // Removing the first timeout
 timeoutCollection.remove(timeoutCollection.get(0).id);
+
+console.log(timeoutCollection.getAll());
+
