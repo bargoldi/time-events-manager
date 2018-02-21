6e1ca860fb6e47cadb68f622851fd2e03890a285
Kevin Shaw <shawmanz32na@gmail.com>
2018-02-21 13:21:36 -0800

    Track completion status of timeouts and allow retrieval of scheduled and completed timeouts
    Refs #3

--- src/timeout/timeout-collection.model.ts
+++ src/timeout/timeout-collection.model.ts
@@ -1,14 +1,21 @@
 import { originalClearTimeout, originalSetTimeout } from '../overrides/override'
+import { TimeoutStatus } from './timeout-status.model';
 import { Timeout } from './timeout.model';
 
 export class TimeoutCollection {
 	private _timeoutCollection: Timeout[] = [];
 
 	public add(handler: any, timeout?: any, ...args: any[]) {
-		let id = originalSetTimeout.apply(window, [handler, timeout, args]);
-		this._timeoutCollection.push({id, handler, timeout, arguments: args, timestamp: Date.now()});
-
+		let id = originalSetTimeout.apply(window, [this._getWrappedHandler(handler), timeout, args]);
+		this._timeoutCollection.push(new Timeout(id, handler, timeout, args));
 		return id;
+	}
+
+	private _getWrappedHandler(handler: Function): Function {
+		return (() => {
+			this._timeoutCollection[this._getTimeoutIndexByHandler(handler)].status = TimeoutStatus.Completed;
+			return handler.apply(window);
+		});
 	}
 
 	public remove(id: number): void {
@@ -23,6 +30,18 @@ export class TimeoutCollection {
 
 	public get(index: number): Timeout {
 		return this._timeoutCollection[index];
+	}
+
+	public getScheduled(): Timeout[] {
+		return this._timeoutCollection.filter((value: Timeout) => {
+			return value.status == TimeoutStatus.Scheduled;
+		});
+	}
+
+	public getCompleted(): Timeout[] {
+		return this._timeoutCollection.filter((value: Timeout) => {
+			return value.status == TimeoutStatus.Completed;
+		});
 	}
 
 	public getAll(): Timeout[] {
@@ -44,6 +63,16 @@ export class TimeoutCollection {
 	private _getTimeoutIndexById(timeoutId: number): number {
 		for (let i = 0; i < this._timeoutCollection.length; i++) {
 			if (this._timeoutCollection[i].id === timeoutId) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	private _getTimeoutIndexByHandler(handler: Function): number {
+		for (let i = 0; i < this._timeoutCollection.length; i++) {
+			if (this._timeoutCollection[i].handler === handler) {
 				return i;
 			}
 		}
--- src/timeout/timeout.model.ts
+++ src/timeout/timeout.model.ts
@@ -1,7 +1,19 @@
-export abstract class Timeout {
+import { TimeoutStatus } from './timeout-status.model';
+
+export class Timeout {
     id: number;
     handler: Function;
     timeout: number;
     arguments: any[];
     timestamp: number;
+    status: TimeoutStatus;
+
+    constructor(id: number, handler: Function, timeout: number, ...args: any[]) {
+        this.id = id;
+        this.handler = handler;
+        this.timeout = timeout;
+        this.arguments = args;
+        this.timestamp = Date.now();
+        this.status = TimeoutStatus.Scheduled;
+    }
 }
